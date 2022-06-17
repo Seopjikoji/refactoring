6e836b3d628ec2e4a373bb57143b2af2c09763dd
seopjikoji <ctensoreo@gmail.com>
2022-06-17 15:00:42 +0900

    함수 선언 바꾸기, /100로직, 바뀐 선언이 적용된 함수 내로 이동

--- chapter1/refactoring1.js
+++ chapter1/refactoring1.js
@@ -7,11 +7,11 @@ function statement(invoice) {
 
         volumeCredits = volumeCreditsFor(perf);
 
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)}\n`
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)}\n`
         totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립포인트: ${volumeCredits}점\n`;
     return result
 
@@ -63,11 +63,11 @@ function volumeCreditsFor(perf) {
     return result
 }
 
-function format(aNumber) {
+function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
         {
             style: "currency", currency: "USD",
             minimumFractionDigits: 2
-        }).format(aNumber);
+        }).format(aNumber / 100);
 }
 console.log(statement(invoice[0], plays))
