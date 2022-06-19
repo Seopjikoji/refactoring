33bd5006473094ca472621a301bd21a21a0b84da
seopjikoji <ctensoreo@gmail.com>
2022-06-19 23:37:55 +0900

    함수 인라인

--- chapter1/refactoring1.js
+++ chapter1/refactoring1.js
@@ -83,8 +83,8 @@ function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
 
         const result = Object.assign({}, aPerformance)
-        result.play = playFor(result);
-        result.amount = amountFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
